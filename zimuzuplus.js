a34b0711c8d25135afb6f82a8ffe2ef5205bc41e
LisonFan <lisonfan1996@gmail.com>
2020-01-04 00:28:47 +0800

    Update zimuzuplus.js

--- zimuzuplus.js
+++ zimuzuplus.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name			字幕组字幕下载页面扩展脚本
-// @icon			http://www.zmz2019.com/favicon.ico
-// @author			LisonFan
-// @version			1.0
-// @description		给字幕组的字幕下载页面增加一个字幕版本对应的视频文件下载链接
-// @match			http://www.zmz2019.com/subtitle/*
-// @grant			none
-// @license			MIT License
+// @name            字幕组字幕下载页面扩展脚本
+// @icon            http://www.zmz2019.com/favicon.ico
+// @author          LisonFan
+// @version         1.0
+// @description     给字幕组的字幕下载页面增加一个字幕版本对应的视频文件下载链接
+// @match           http://www.zmz2019.com/subtitle/*
+// @grant           none
+// @license         MIT License
 // @require         https://lib.baomitu.com/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
